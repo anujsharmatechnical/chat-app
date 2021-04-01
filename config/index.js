9a6700a1224dfb531d5f4841cf05cf31bd05c63a
anujsharmatechnical <anuj19sharma29@gmail.com>
2021-04-01 11:11:04 +0530

    first commit

---
+++ config/index.js
@@ -0,0 +1,8 @@
+const config = {
+  db: {
+    url:
+      "mongodb+srv://Anuj:1234@cluster0.z2iuf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    name: "chatdb",
+  },
+};
+export default config;
---
+++ routes/user.js
@@ -0,0 +1,13 @@
+// // import express, { Router } from "express";
+// const express = require("express")
+// const Router = eexpress.Router()
+
+// //controllers
+// import user from "../controllers/user.js";
+// const router = express.Router();
+// router.get("/", user.onGetAllUsers);
+// router.post("/", user.onCreateUser);
+// router.get("/:id", user.onGetUserById);
+// router.delete("/id", user.onDeleteUserById);
+
+// export default router;
